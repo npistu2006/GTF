c5d5b27526b2fe27f197de599fceacd2f3555ae9
Partisan1 <128032116+Partisan1@users.noreply.github.com>
2024-11-20 09:05:02 +0100

    changes, elérések

---
+++ app.js
@@ -0,0 +1,156 @@
+const countries = [
+    {
+        flag: "magyarorszag.png",
+        name: "Magyarország"
+    },
+    {
+        flag: "nemetorszag.png",
+        name: "Németország"
+    },
+    {
+        flag: "olaszorszag.png",
+        name: "Olaszország"
+    },
+    {
+        flag: "csehorszag.png",
+        name: "Csehország"
+    },
+    {
+        flag: "szlovakia.png",
+        name:  "Szlovákia"
+    },
+    {
+        flag: "romania.png",
+        name: "Románia"
+    },
+    {
+        flag: "horvatorszag.png",
+        name: "Horvátország"
+    },
+    {
+        flag: "alban.png",
+        name: "Albánia"
+    },
+    {
+        flag: "szerbia.png",
+        name: "Szerbia"
+    },
+    {
+        flag: "bulgaria.png",
+        name: "Bulgária"
+    },
+    {
+        flag: "gorogorszag.png",
+        name: "Görögország"
+    },
+    {
+        flag: "franciaorszag.png",
+        name: "Franciaország"
+    },
+    {
+        flag: "hollandia.png",
+        name: "Hollandia"
+    },
+    {
+        flag: "belgium.png",
+        name: "Belgium"
+    },
+    {
+        flag: "spanyolorszag.png",
+        name: "Spanyolország"
+    },
+    {
+        flag: "portugalia.png",
+        name: "Portugália"
+    },
+    {
+        flag: "norvegia.png",
+        name: "Norvégia"
+    },
+    {
+        flag: "svajc.png",
+        name: "Svédország"
+    },
+    {
+        flag: "finnorszag.png",
+        name: "Finnország"
+    },
+    {
+        flag: "oroszorszag.png",
+        name: "Oroszország"
+    },
+    {
+        flag: "ukrajna.png",
+        name: "Ukrajna"
+    },
+    {
+        flag: "moldova.png",
+        name: "Moldova"
+    },
+    {
+        flag: "lengyelorszag.png",
+        name: "Lengyelország"
+    },
+    {
+        flag: "esztorszag.png",
+        name: "Észtország"
+    },
+    {
+        flag: "lettorszag.png",
+        name: "Lettország"
+    },
+    {
+        flag: "litvania.png",
+        name: "Litvánia"
+    },
+    {
+        flag: "ausztria.png",
+        name: "Ausztria"
+    },
+    {
+        flag: "svajc.png",
+        name: "Svájc"
+    },
+    {
+        flag: "feheroroszorszag.png",
+        name: "Fehéroroszország"
+    },
+    {
+        flag: "luxemburg.png",
+        name: "Luxemburg"
+    },
+    {
+        flag: "andorra.png",
+        name: "Andorra"
+    },
+    {
+        flag: "egyesult_kiralysag.png",
+        name: "Egyesült Királyság"
+    }
+
+
+]
+
+function getRandomNumber() {
+    let newNum =  Math.floor(Math.random() * countries.length);
+    return newNum;
+}
+
+function getCountryName(index) {
+    let countryName = countries[index].name;
+    return countryName;
+}   
+
+function getFlagImage(index) {
+    let countryFlag = countries[index].flag;
+    return countryFlag;
+}
+
+let currentNumber = getRandomNumber();
+let currentCountryName = getCountryName(currentNumber);
+let currentFlag = getFlagImage(currentNumber);
+
+console.log(currentNumber)
+console.log(currentCountryName)
+console.log(currentFlag)
+
